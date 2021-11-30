a7b8b2ae4d17531725def49ed102a57499d51c8e
TYuUWO <tyu263@uwo.ca>
2021-11-30 04:29:07 -0500

    initialized project

---
+++ Client.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+
+using namespace std;
+
+int main(void){
+	string inputData;
+	string serverMsg;
+	string ipAddr;
+	const int portNo = 3000;
+	bool socketCreated = false;
+	
+	cout << "Welcome to the Game" << endl << "Please enter the server IP" <<endl;
+	cin >> ipAddr;
+	
+	
+	while(inputData != "quit" && socketCreated){
+		
+	}
+	return 0;
+}
---
+++ Server.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+
+using namespace std;
+
+int serverThread(){
+	//main server thread
+	
+	//connect to clients via sockets
+	
+	//create new client thread
+
+}
+
+int clientThread(){
+	//main client thread
+	
+	//interacts with clients
+}
+
+int main(void){
+	string inputData;
+	string serverMsg;
+	const int portNo = 3000;
+	
+	cout << "This is the server." << endl;
+	
+	//create the main server thread
+	
+	//check for server exit
+	
+	//shut down server and send message to terminate clients
+	
+	return 0;
+}
